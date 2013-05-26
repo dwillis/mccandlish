7e077fc7ba4e1f51f50414ba97cc6b2d724c71d4
Derek Willis <dwillis@gmail.com>
2013-05-26 16:19:17 -0400

    added result and reset methods

--- lib/mccandlish/client.rb
+++ lib/mccandlish/client.rb
@@ -1,7 +1,6 @@
 require 'httparty'
 require 'oj'
 require 'cgi'
-require 'net/http'
 
 module Mccandlish
 
@@ -9,19 +8,23 @@ module Mccandlish
     
     include HTTParty
     
-    attr_reader :web_url, :snippet, :lead_paragraph, :abstract, :print_page, :blog, :source, :multimedia, :headline,
-    :keywords, :pub_date, :document_type, :news_desk, :byline, :type_of_material, :id, :word_count, :query_filters, 
-    :date_filers, :sort, :page, :facet_field_filters, :facet_filter, :search, :api_key, :params
+    attr_reader :web_url, :snippet, :lead_paragraph, :abstract, :print_page, :blog, :source, :multimedia, :headline_seo,
+    :keywords, :pub_date, :document_type, :news_desk, :byline, :type_of_material, :id, :word_count, :sort, :page, 
+    :facet_field_filters, :facet_filter, :query, :api_key, :params, :results, :byline, :section_name, :subsection_name,
+    :headline_main, :headline_print, :query_filters, :result, :uri
     
-    def initialize(query, api_key=nil)
+    def initialize(api_key=nil)
       @api_key = api_key
-      @params = {'q' => query}
-      @query_filters = {}
-      @date_filters = {}
-      @sort = {}
-      @page = {}
-      @facet_field_filters = {}
-      @facet_filter = {}
+      @params = {}
+      @query_filters = []
+      @uri
+    end
+    
+    # clears out params, query_filters 
+    def reset
+      @params = {}
+      @query_filters = []
+      self
     end
 
     ##
@@ -31,14 +34,14 @@ module Mccandlish
 		end
 
 		def prepare_params(params, api_key)
-		  params.map {|k,v| "#{CGI.escape(k)}=#{CGI.escape(v.to_s)}"}.join('&')+"&api-key=#{api_key}"
+		  params.map {|k,v| k+':'+v.to_s}.join('&')+"&api-key=#{api_key}"
 		end
 
     def invoke(params={})
 			raise "You must initialize the API key before you run any API queries" if self.api_key.nil?
 			full_params = prepare_params(params, api_key=self.api_key)
-			uri = build_request_url(full_params)
-			response = HTTParty.get(uri)
+			@uri = build_request_url(full_params)
+			response = HTTParty.get(@uri)
 			check_response(response)
     end
 
@@ -46,6 +49,7 @@ module Mccandlish
       raise "Authentication Error" if response.code == 403
       raise "Bad Request" if response.code == 400
       raise "Server Error" if response.code == 500
+      raise "Timeout" if response.code == 504
       if response.code == 200
         Oj.load(response.body)
       else
@@ -53,20 +57,54 @@ module Mccandlish
       end
     end
     
-    def search(params)
+    def result
       invoke(params)
     end
     
-    def dates(begin_date, end_date)
-      date_filters['begin_date'] = begin_date if begin_date
-      date_filters['end_date'] = end_date if end_date
-      params.merge!(date_filters)
-      self.search(self.params)
+    def query(query)
+      params['q'] = CGI.escape(query)
+      params.merge!({'fq' => query_filters.uniq.join("+AND+")})
+      self
     end
     
-    def word_count(start, finish)
-      
-      
+    # newest or oldest
+    def sort(sort)
+      params['sort'] = sort
+      self
+    end
+    
+    # response has maximum of 10 items, page is for pagination of results
+    def page(page)
+      params['page'] = page
+    end
+    
+    def dates(begin_date, end_date)
+      params['begin_date'] = begin_date if begin_date
+      params['end_date'] = end_date if end_date
+      self
+    end
+    
+    # Full day name: Monday, Tuesday, Wednesday, etc.
+    def day_of_week(day)
+      query_filters << "day_of_week:'#{day}'"
+      self
+    end
+    
+    # YYYY-MM-DD format
+    def date(date)
+      query_filters << "pub_date:#{date}"
+      self
+    end
+    
+    def year(year)
+      query_filters << "pub_year:#{year}"
+      self
+    end
+    
+    def location(location)
+      loc = CGI.escape(location)
+      query_filters << "glocations:'#{loc}'"
+      self
     end
       
   end
